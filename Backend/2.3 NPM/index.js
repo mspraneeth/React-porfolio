8129c8f8f67955183b5d1fed3eafdb66cb2135c6
mspraneeth <68723413+mspraneeth@users.noreply.github.com>
2024-07-18 11:30:14 -0500

    Add files via upload

---
+++ Backend/2.3 NPM/index.js
@@ -0,0 +1,8 @@
+// var generateName = require('sillyname');
+
+import superheroes from "superheroes";
+
+const name = superheroes.random();
+// var sillyName = generateName();
+console.log(`I am a ${name}.`);
+
